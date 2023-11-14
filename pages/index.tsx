8d4f8de8609d5308f2439f427c9eff4696d17375
tatobig <tatobig@gmail.com>
2023-11-14 11:16:30 -0600

    Fixed links

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,7 +82,7 @@ export default function Example() {
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <a
-              href="http://localhost:8000/admin/login"
+              href="http://localhost:8000/admin"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
               Ingresar <span aria-hidden="true">&rarr;</span>
@@ -130,7 +130,7 @@ export default function Example() {
                 </div>
                 <div className="py-6">
                   <a
-                    href="http://localhost:8000/admin/login"
+                    href="http://localhost:8000/admin"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Ingresar
@@ -254,7 +254,7 @@ export default function Example() {
               </h2>
               <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
                 <a
-                  href="http://localhost:8000/admin/login"
+                  href="http://localhost:8000/admin"
                   className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                 >
                   Ingresar
